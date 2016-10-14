02b0498701442e71f11e29616210ba2632c9da44
jcapitan <jcapitan@us.es>
2016-10-14 23:13:28 +0200

    Launch files moved to teresa_common

--- upo_launchers/teresa_robot3/nav_behavior_elderly_center.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-roslaunch upo_launchers real_nav_behavior.launch robot:=teresa_robot3 scenario:=elderly_center xtion_nav:=false &
-echo $!
-sleep 5
-
-#People localisation
-roslaunch uva_launchers combined_tracking.launch
-
